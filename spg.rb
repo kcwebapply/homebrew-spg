af193ddf781f023c08f35b18f55c36fb051cd8c9
Keishi wada <kcwebapply@gmail.com>
2019-02-01 22:56:51 +0900

    Update spg.rb

--- spg.rb
+++ spg.rb
@@ -4,18 +4,17 @@
 class Spg < Formula
   desc "Save memo easily on terminal"
   homepage "https://github.com/kcwebapply/spg"
-  url "https://github.com/kcwebapply/spg/archive/1.0.2.tar.gz"
+  url "https://github.com/kcwebapply/spg/archive/1.0.3.tar.gz"
   sha256 "28af272571e3d8a9aa4a555655e6e3a6ca275773a538ea314d22bec590891ad6"
   depends_on "dep" => :build
   depends_on "go" => :build
 
   def install
-     ENV["GOPATH"] = buildpath
+    ENV["GOPATH"] = buildpath
     spg_path = buildpath/"src/github.com/kcwebapply/spg/"
     spg_path.install buildpath.children
 
     cd spg_path do
-       system 'go', 'get', 'github.com/jessevdk/go-flags'
       system "dep", "ensure", "-vendor-only"
       system "go", "build"
       bin.install "spg"
