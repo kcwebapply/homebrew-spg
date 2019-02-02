ff9590b922146f52be09c676e5711d5fda36bcb5
Keishi wada <kcwebapply@gmail.com>
2019-02-02 09:45:39 +0900

    Update spg.rb

--- spg.rb
+++ spg.rb
@@ -4,7 +4,7 @@
 class Spg < Formula
   desc "Save memo easily on terminal"
   homepage "https://github.com/kcwebapply/spg"
-  url "https://github.com/kcwebapply/spg/archive/1.0.3.tar.gz"
+  url "https://github.com/kcwebapply/spg/archive/1.0.4.tar.gz"
   sha256 "dced1c9465ed6d86aa122e860cc610fab308ae3e3eb3554a7067097e8a2b0b91"
   depends_on "dep" => :build
   depends_on "go" => :build
